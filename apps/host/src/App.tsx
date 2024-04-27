9314dc327cd6c94c2955482f2c6b89570e607e2c
Esteban Burgos <estebanburgos85@gmail.com>
2024-04-27 18:29:23 -0300

    add some libs

--- apps/host/src/App.tsx
+++ apps/host/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Host</h1>
       <div className="card">
         <Button />
         <p>
